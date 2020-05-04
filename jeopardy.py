20b84ab8d05d0d5d155e349710e788e081148acc
Alexander French <alexander.french.uk@gmail.com>
2020-05-04 23:11:23 +0100

    added function which filters questions by word

--- jeopardy.py
+++ jeopardy.py
@@ -1,2 +1,15 @@
 import pandas as pd
 pd.set_option('display.max_colwidth', -1)
+
+#Loading data and reformating the columns
+show_data = pd.read_csv('jeopardy.csv')
+show_data = show_data.rename(columns = {" Air Date": "Air Date", " Round" : "Round", " Category": "Category", " Value": "Value", " Question":"Question", " Answer": "Answer"})
+
+#Finds questions which contain a certain word
+def find_in_q(words):
+    filter_func = lambda x: all(i.lower() in x.lower() for i in words)
+    return show_data[show_data['Question'].apply(filter_func)]
+
+#Creating a test for our function
+test_words = ["king", "england"]
+print(find_in_q(test_words))

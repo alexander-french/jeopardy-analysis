0277eca3d73830cc01053ce257fa941840b93bb6
Alexander French <alexander.french.uk@gmail.com>
2020-05-04 23:26:46 +0100

    added column with float equivalents of values

--- jeopardy.py
+++ jeopardy.py
@@ -11,5 +11,9 @@ def find_in_q(words):
     return show_data[show_data['Question'].apply(filter_func)]
 
 #Creating a test for our function
-test_words = ["king", "england"]
+test_words = ['king', 'england']
 print(find_in_q(test_words))
+
+#Column which contains the float equivalents of question values
+convert_to_float = lambda x: float(x[1:].replace(',', '',) if x != 'None' else 0)
+show_data['Float Value'] = show_data['Value'].apply(convert_to_float)
